5c76dbbbd127d1697e0ccfdefdefa50a6cf1ac10
Liborio Castañeda Valencia <74385144+LiborioCastaneda99@users.noreply.github.com>
2024-07-11 01:59:37 -0500

    Update global.js

--- src/config/global.js
+++ src/config/global.js
@@ -114,11 +114,11 @@ export default {
         titulo: 'Descargar PDF',
         download: 'downloads/722103_CF19_DU.pdf',
       },
-      // {
-      //   icono: 'fas fa-download',
-      //   titulo: 'Descargar material',
-      //   download: 'downloads/material.zip',
-      // },
+      {
+        icono: 'fas fa-download',
+        titulo: 'Descargar material',
+        download: 'downloads/material.zip',
+      },
       {
         icono: 'far fa-registered',
         titulo: 'Créditos',
